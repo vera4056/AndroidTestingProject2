b3e059bc164b4614ec1db10bf31ce4d11c9a0ca2
vera4056 <103599166+vera4056@users.noreply.github.com>
2024-09-10 21:19:10 +0300

    initial commit

---
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/tests/AboutTests.java
@@ -0,0 +1,70 @@
+package ru.iteco.fmhandroid.ui.tests;
+
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+import ru.iteco.fmhandroid.ui.AppActivity;
+import ru.iteco.fmhandroid.ui.steps.AboutSteps;
+import ru.iteco.fmhandroid.ui.steps.LoginSteps;
+import ru.iteco.fmhandroid.ui.steps.MainMenuSteps;
+
+@LargeTest
+@RunWith(AllureAndroidJUnit4.class)
+
+public class AboutTests {
+    LoginSteps loginSteps = new LoginSteps();
+    AboutSteps aboutSteps = new AboutSteps();
+    MainMenuSteps mainMenuSteps = new MainMenuSteps();
+
+    @Rule
+    public ActivityScenarioRule<AppActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(AppActivity.class);
+
+    @Before
+    public void aboutScreenLoad() {
+
+        try {
+            loginSteps.waitAuthorizationPage();
+            loginSteps.validLogin();
+        } catch (Exception e) {
+            mainMenuSteps.mainScreenLoad();
+        }
+
+    }
+
+    @Test
+    public void openAboutSectionTest() {
+        aboutSteps.goToAboutSection();
+
+    }
+
+    @Test
+    public void displayAboutSectionsTest() {
+        aboutSteps.displayAboutSection();
+    }
+
+    @Test
+    public void goToPrivacyPolicyTest() {
+        aboutSteps.linkToPrivacyPolicy();
+
+    }
+
+    @Test
+    public void goToTermsOfUseTest() {
+        aboutSteps.linkToTermsOfUse();
+
+    }
+
+    @Test
+    public void backToNewsTest() {
+        aboutSteps.backToNews();
+
+    }
+}
